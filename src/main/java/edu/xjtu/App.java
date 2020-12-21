53d10bd335f8828fd66cd2b2a25039e503e0e42b
Ruixin Li <l94212wow@gmail.com>
2020-12-22 00:09:40 +0800

    initial commit

---
+++ src/main/java/edu/xjtu/App.java
@@ -0,0 +1,13 @@
+package edu.xjtu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
