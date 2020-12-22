82a9744a310ef5ef1330af1719eba7e6151afe4b
李瑞昕 <454039249@qq.com>
2020-12-22 16:27:19 +0800

    Add log4j.properties

--- src/main/java/edu/xjtu/App.java
+++ src/main/java/edu/xjtu/App.java
@@ -1,13 +1,19 @@
 package edu.xjtu;
 
-/**
- * Hello world!
- *
- */
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class App 
 {
+    private static final Logger logger;
+
+    static {
+        logger = LogManager.getLogger(App.class);
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        logger.info(System.nanoTime());
     }
 }
