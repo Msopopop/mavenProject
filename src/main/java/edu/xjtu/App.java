207a2427bd2bc61cd7ec632b3658716d5f6eee5a
Ruixin Li <l94212wow@gmail.com>
2020-12-22 19:49:51 +0800

    ==

--- src/main/java/edu/xjtu/App.java
+++ src/main/java/edu/xjtu/App.java
@@ -4,10 +4,15 @@ package edu.xjtu;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public boolean testMethod(int a)
+    {
+        return a>0;
     }
 }
